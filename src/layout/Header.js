d29652f443a6e630bfe5184bc6f5dfa47fcf8546
ThioKDG <kim2006207@gmail.com>
2023-03-21 01:45:48 +0900

    0321 검색 기능 구현 중

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -5,6 +5,7 @@ import './layStyles/headStyle.scss';
 
 function Header() {
    const [navi, setNavi] = useState(false);
+   const [btnOn, setBtnOn] = useState(false);
    const searchRef = useRef(null);
    
    useEffect(() => {
@@ -23,7 +24,7 @@ function Header() {
                   <span className='hiddenWord'>로고</span>
                </Link>
             </h1>
-            <p className='searchBtn'>
+            <p className='searchBtn' onClick={() => {btnOn ? setBtnOn(false) : setBtnOn(true)}}>
                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill='#fff'>
                   <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/>
                </svg>
@@ -31,9 +32,9 @@ function Header() {
          </nav>
          <section className="searchArea">
             <div className="boxWrapper">
-               <p className='pTitle'>검색하실 영화이름을 입력해주세요</p>
+               <p className='pTitle'>영화 제목으로 검색해보세요</p>
                <div className='search'>
-                  <input className='inputSearch' ref={searchRef} type="text" />
+                  <input className='inputSearch' ref={searchRef} type="text" placeholder='제목입력...' />
                   <button>검색</button>
                </div>
             </div>
