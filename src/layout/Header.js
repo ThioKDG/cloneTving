722550a1739224c49e9539345300a82fe8e4a118
ThioKDG <kim2006207@gmail.com>
2023-03-21 13:43:35 +0900

    0321 검색 기능 구현중

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,11 +1,14 @@
+import axios from 'axios';
 import { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import './layStyles/headStyle.scss';
 
 function Header() {
+   const key = process.env.REACT_APP_TDMB_KEY;
    const [navi, setNavi] = useState(false);
    const [btnOn, setBtnOn] = useState(false);
+   const [search, setSearch] = useState([]);
    const searchRef = useRef(null);
    
    useEffect(() => {
@@ -13,9 +16,14 @@ function Header() {
          window.scrollY >= 100 ? setNavi(true) : setNavi(false);
       }
       window.addEventListener('scroll', scrollHandler);
-      
+      const getSearch = async() => {
+         const searchPage = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${key}&language=ko-KR&query=${ddd}page=1&include_adult=false`);
+         setSearch(searchPage.data);
+      }
+      getSearch();
       return () => window.removeEventListener('scroll', scrollHandler);
-   }, [])
+   }, []);
+   console.log(search);
    return (
       <header>
          <nav className={navi ? 'navWrapper scrolled' : 'navWrapper'}>
@@ -30,7 +38,7 @@ function Header() {
                </svg>
             </p>
          </nav>
-         <section className="searchArea">
+         <section className={btnOn ? 'searchArea on' : 'searchArea'}>
             <div className="boxWrapper">
                <p className='pTitle'>영화 제목으로 검색해보세요</p>
                <div className='search'>
