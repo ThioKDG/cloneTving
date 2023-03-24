b25081321e7b86428af3030a7dcab0f32af46dfb
ThioKDG <kim2006207@gmail.com>
2023-03-25 02:09:04 +0900

    0325 검색페이지 마크업 및 클릭이벤트 조정중

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,17 +1,22 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 import './compStyle/searchStyle.scss';
 
 function Search() {
     const key = process.env.REACT_APP_TDMB_KEY;
     const imageUrl = 'https://image.tmdb.org/t/p/original';
+    const navigation = useNavigate();
     
     const { name } = useParams();
     const [ searchResult, setSearchResult ] = useState([]);
     
     console.log(name);
+    
+    const toDetail = () =>{
+        navigation(`/cont/${searchResult.id}`);
+    }
     
     useEffect(() => {
         const getResult = async() => {
@@ -27,16 +32,16 @@ function Search() {
             <section className='resultBox'>
                 <div className="wrapper">
                     <div className='searchArea'>
-                        <h2>{}</h2>
+                        <h2 className='resultH2'>{name} 으로 검색한 영화들입니다.</h2>
                     </div>
                     <div className="resultArea">
                         <ul className='areaList'>
                             {searchResult && searchResult.map((item) => (
-                                    <li key={item.id}>
-                                        <div>
+                                    <li key={item.id} onClick={toDetail()}>
+                                        <div className='posterWrapper'>
                                             <img src={ imageUrl + item.poster_path } alt={item.name} />
-                                            <h3>{item.name}</h3>
                                         </div>
+                                        <h3 className='resultH3'>{item.title}</h3>
                                     </li>
                                     
                             ))}
